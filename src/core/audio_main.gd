26c38656dbe82c022202cb3935b7eefba3e52883
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-01-03 23:04:56 -0800

    Re-arrange directories

--- src/core/audio_main.gd
+++ src/core/audio_main.gd
@@ -11,52 +11,52 @@ extends Node2D
 
 
 func _enter_tree() -> void:
-	G.audio = self
+    G.audio = self
 
 
 func play_click_sound() -> void:
-	if not %ClickStreamPlayer.playing:
-		%ClickStreamPlayer.play()
+    if not %ClickStreamPlayer.playing:
+        %ClickStreamPlayer.play()
 
 
 func fade_to_menu_theme() -> void:
-	fade_out(%MainThemeStreamPlayer)
-	fade_in(%MenuThemeStreamPlayer, menu_theme_volume)
+    fade_out(%MainThemeStreamPlayer)
+    fade_in(%MenuThemeStreamPlayer, menu_theme_volume)
 
 
 func fade_to_main_theme() -> void:
-	fade_out(%MenuThemeStreamPlayer)
-	fade_in(%MainThemeStreamPlayer, main_theme_volume)
+    fade_out(%MenuThemeStreamPlayer)
+    fade_in(%MainThemeStreamPlayer, main_theme_volume)
 
 
 func fade_in(stream_player: AudioStreamPlayer, volume: float) -> void:
-	if G.settings.mute_music:
-		volume = mute_volume
-	
-	if not stream_player.playing:
-		stream_player.volume_db = mute_volume
-		# This is similar to calling play(), except play() resets playback position to zero.
-		stream_player.stream_paused = false
-	
-	var tween := create_tween()
-	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-	tween.tween_property(stream_player, "volume_db", volume, theme_fade_duration_sec)
+    if G.settings.mute_music:
+        volume = mute_volume
 
-	await tween.step_finished
-	# Ensure the stream is still playing, just in case we somehow end up with overlapping tweens
-	# (the latest tween should end up winning).
-	stream_player.stream_paused = false
+    if not stream_player.playing:
+        stream_player.volume_db = mute_volume
+        # This is similar to calling play(), except play() resets playback position to zero.
+        stream_player.stream_paused = false
+
+    var tween := create_tween()
+    tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+    tween.tween_property(stream_player, "volume_db", volume, theme_fade_duration_sec)
+
+    await tween.step_finished
+    # Ensure the stream is still playing, just in case we somehow end up with overlapping tweens
+    # (the latest tween should end up winning).
+    stream_player.stream_paused = false
 
 
 func fade_out(stream_player: AudioStreamPlayer) -> void:
-	if not stream_player.playing:
-		return
-	
-	var tween := create_tween()
-	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-	tween.tween_property(stream_player, "volume_db", mute_volume, theme_fade_duration_sec)
+    if not stream_player.playing:
+        return
 
-	await tween.step_finished
-	# Ensure the stream is still playing, just in case we somehow end up with overlapping tweens
-	# (the latest tween should end up winning).
-	stream_player.stream_paused = true
+    var tween := create_tween()
+    tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+    tween.tween_property(stream_player, "volume_db", mute_volume, theme_fade_duration_sec)
+
+    await tween.step_finished
+    # Ensure the stream is still playing, just in case we somehow end up with overlapping tweens
+    # (the latest tween should end up winning).
+    stream_player.stream_paused = true
--- src/core/screens_main.gd
+++ src/core/screens_main.gd
@@ -3,61 +3,61 @@ extends Node2D
 
 
 enum ScreenType {
-	MAIN_MENU,
-	GAME_OVER,
-	WIN,
-	PAUSE,
-	GAME,
+    MAIN_MENU,
+    GAME_OVER,
+    WIN,
+    PAUSE,
+    GAME,
 }
 
 var current_screen := ScreenType.MAIN_MENU
 
 
 func _enter_tree() -> void:
-	G.screens = self
+    G.screens = self
 
 
 func open_screen(screen_type: ScreenType) -> void:
-	current_screen = screen_type
+    current_screen = screen_type
 
-	get_tree().paused = screen_type != ScreenType.GAME
+    get_tree().paused = screen_type != ScreenType.GAME
 
-	G.main_menu_screen.visible = screen_type == ScreenType.MAIN_MENU
-	G.game_over_screen.visible = screen_type == ScreenType.GAME_OVER
-	G.win_screen.visible = screen_type == ScreenType.WIN
-	G.pause_screen.visible = screen_type == ScreenType.PAUSE
-	
-	var ends_game := [
-		ScreenType.MAIN_MENU,
-		ScreenType.GAME_OVER,
-		ScreenType.WIN,
-	].has(screen_type)
-	if ends_game:
-		G.game_panel.end_game()
-		
-	var plays_menu_theme := [
-		ScreenType.MAIN_MENU,
-		ScreenType.GAME_OVER,
-		ScreenType.WIN,
-		ScreenType.PAUSE,
-	].has(screen_type)
-	if plays_menu_theme:
-		G.audio.fade_to_menu_theme()
-		
-	var plays_main_theme := [ScreenType.GAME].has(screen_type)
-	if plays_main_theme:
-		G.audio.fade_to_main_theme()
+    G.main_menu_screen.visible = screen_type == ScreenType.MAIN_MENU
+    G.game_over_screen.visible = screen_type == ScreenType.GAME_OVER
+    G.win_screen.visible = screen_type == ScreenType.WIN
+    G.pause_screen.visible = screen_type == ScreenType.PAUSE
 
-	match screen_type:
-		ScreenType.MAIN_MENU:
-			G.main_menu_screen.on_open()
-		ScreenType.GAME_OVER:
-			G.game_over_screen.on_open()
-		ScreenType.WIN:
-			G.win_screen.on_open()
-		ScreenType.PAUSE:
-			G.pause_screen.on_open()
-		ScreenType.GAME:
-			G.game_panel.on_return_from_screen()
+    var ends_game := [
+        ScreenType.MAIN_MENU,
+        ScreenType.GAME_OVER,
+        ScreenType.WIN,
+    ].has(screen_type)
+    if ends_game:
+        G.game_panel.end_game()
 
-	G.hud.update_visibility()
+    var plays_menu_theme := [
+        ScreenType.MAIN_MENU,
+        ScreenType.GAME_OVER,
+        ScreenType.WIN,
+        ScreenType.PAUSE,
+    ].has(screen_type)
+    if plays_menu_theme:
+        G.audio.fade_to_menu_theme()
+
+    var plays_main_theme := [ScreenType.GAME].has(screen_type)
+    if plays_main_theme:
+        G.audio.fade_to_main_theme()
+
+    match screen_type:
+        ScreenType.MAIN_MENU:
+            G.main_menu_screen.on_open()
+        ScreenType.GAME_OVER:
+            G.game_over_screen.on_open()
+        ScreenType.WIN:
+            G.win_screen.on_open()
+        ScreenType.PAUSE:
+            G.pause_screen.on_open()
+        ScreenType.GAME:
+            G.game_panel.on_return_from_screen()
+
+    G.hud.update_visibility()
